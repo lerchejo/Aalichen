a394c6a9248502f7566b3851fc46e30ebe241762
jl9767s <joachim.lerchenmueller@alumni.fh-aachen,de>
2024-07-05 15:40:04 +0200

    Test

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,19 +8,47 @@ public class Bullet : MonoBehaviour
     public int damage = 10;
     protected Vector2 targetPosition;
     protected Vector2 direction;
-    
+
     private AudioSource bulletSound;
     private AudioSource ImpactSound;
+    private AudioSource bulletMiss;
+    private AudioSource bulletNearMiss; // AudioSource for the near miss sound
+
+    private Transform player; // Transform of the player
+    public float nearMissDistance = 1.0f; // Distance threshold for a near miss
 
     private void Start()
     {
         ImpactSound = GameObject.Find("ImpactSound").GetComponent<AudioSource>();
         bulletSound = GameObject.Find("GunshotSound").GetComponent<AudioSource>();
-        
+        bulletMiss = GameObject.Find("BulletMissSound").GetComponent<AudioSource>();
+        bulletNearMiss = GameObject.Find("BulletNearMissSound").GetComponent<AudioSource>(); // Get the AudioSource for the near miss sound
         bulletSound.Play();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform; // Get the Transform of the player
+        StartCoroutine(CheckForNearMiss());
+
+        
     }
 
+    
+    IEnumerator CheckForNearMiss()
+    {
+        while (true)
+        {
+            // Calculate the distance to the player
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
+            // If the distance to the player is less than the near miss distance, play the near miss sound
+            if (distanceToPlayer < nearMissDistance && !bulletNearMiss.isPlaying    )
+            {
+                bulletNearMiss.Play();
+            }
+
+            // Wait for 0.1 seconds before the next check
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
     public void Seek(Vector2 _targetPosition)
     {
         // Store the target's position
@@ -39,6 +68,15 @@ public class Bullet : MonoBehaviour
 
         // Move the bullet in the stored direction
         transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+        // Calculate the distance to the player
+     //  float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+     //  // If the distance to the player is less than the near miss distance, play the near miss sound
+     //  if (distanceToPlayer < nearMissDistance)
+     //  {
+     //      bulletNearMiss.Play();
+     //  }
     }
 
     protected void OnTriggerEnter2D(Collider2D hitInfo)
@@ -56,6 +94,7 @@ public class Bullet : MonoBehaviour
         }
         else
         {
+            bulletMiss.Play();
             Destroy(gameObject);
         }
     }
